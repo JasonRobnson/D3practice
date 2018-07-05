271e6e72120369b2cd84bf20a7e228a048d6a951
unknown <bubbastinx@gmail.com>
2018-07-05 11:31:36 -0600

    debugging line graph, able to get Yaxis working, but cant get Xaxis to display Strings

--- index.js
+++ index.js
@@ -38,7 +38,15 @@
      
 /// THIRD TUTORIAL 
 
-//  let dataset = [40, 250, 56, 120, 180, 30, 40, 120, 160]
+// let (bargraph)dataset = [0, 250, 56, 120, 180, 30, 40 ]
+
+
+ let dataset = [0, 16, 32, 64, 96, 112, 128, 144 ]
+
+
+ let dayDataset = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+//  let dayDataset = [ 0, 1, 2, 3, 4, 5, 6];
 
 //  let svgWidth = 500, svgHeight = 300, barPadding = 5;
 //  let barWidth = (svgWidth / dataset.length);
@@ -103,3 +111,65 @@
 // svg.append("g")
 //     .attr("transform", "translate(50, " + xAxisTranslate +")")
 //     .call(x_axis);
+
+
+    //Line Chart Tutorial 
+
+
+const drawingChart = (dataset, dayDataset) => {
+    let svgWidth = 600; 
+    let svgHeight = 400;
+    let margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    let width = svgWidth - margin.left - margin.right; 
+    let height = svgHeight - margin.top - margin.bottom;
+
+    let svg = d3.select('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+    let g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    let x = d3.scale.ordinal()
+        .domain(['Monday', 'Sunday'])
+        .range([0, width]);
+    
+    let y = d3.scaleLinear()
+        .rangeRound([0, height]);
+
+    let line = d3.line()
+        .x((dayDataset) => { return x(dayDataset)})
+        .y((dataset) => { return y(dataset)})
+        x.domain(d3.extent(dayDataset, (dayDataset) => { return dayDataset}));
+        y.domain(d3.extent(dataset, (dataset) => { return dataset}));
+
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .append("text")
+        .attr("fill", "#000")
+        .select(".domain")
+        .remove();
+
+    g.append("g")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "1em")
+        .attr("text-anchor", "end")
+        .text("Water (Cups)");
+
+    g.append("g")
+        .datum(dataset)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);  
+
+}
+
+drawingChart(dataset, dayDataset)
