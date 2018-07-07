6c086b24b922f09dc3059c0a4c315eafb9d34d9e
unknown <bubbastinx@gmail.com>
2018-07-06 20:12:17 -0600

    did a git reset

--- healthTracker.js
+++ healthTracker.js
@@ -1,44 +1,24 @@
 
 
 // This line deals with setting up the scale on the x and y axis
-let dataset = [40, 250, 56, 120, 180, 30, 40, 120, 160]
+let dataset = [4, 18, 96, 120, 180, 30, 40, 120, 160]
 
 
-let svgWidth = 500, svgHeight = 300;
+// let svgWidth = 500, svgHeight = 300;
 
-let svg = d3.select('svg')
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
-
-let xScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset)])
-    .range([0, svgWidth]);
-
-let yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset)]) 
-    .range([svgHeight, 0]);
-
-let x_axis = d3.axisBottom().scale(xScale);
-let y_axis = d3.axisLeft().scale(yScale);
-
-svg.append("g")
-    .attr("transform", "translate(50, 10)")
-    .call(y_axis);
-
-let xAxisTranslate = svgHeight - 20;
-
-svg.append("g")
-    .attr("transform", "translate(50, " + xAxisTranslate +")")
-    .call(x_axis);
-//End of the scale x and y axis code 
+// let svg = d3.select('svg')
+//     .attr("width", svgWidth)
+//     .attr("height", svgHeight);
 
 
 
 
 
-//BAR CHART BELOW WITH LABELS
 
-let dataset = [40, 250, 56, 120, 180, 30, 40, 120, 160]
+
+// //BAR CHART BELOW WITH LABELS
+
+// let dataset = [40, 250, 56, 120, 180, 30, 40, 120, 160]
 
 let svgWidth = 500, svgHeight = 300, barPadding = 5;
 let barWidth = (svgWidth / dataset.length);
@@ -60,7 +40,7 @@ let barChart = svg.selectAll("rect")
    .attr("transform", (data, index) => {
        let translate = [barWidth * index, 0];
        return "translate("+translate +")"
-   })
+   }).attr("fill", "00ff00")
 
 
    //BarChart Above, chart labels below
@@ -79,3 +59,26 @@ let barChart = svg.selectAll("rect")
         return barWidth * index;
     })
     .attr("fill", "#FF0800") 
+
+
+//     let xScale = d3.scaleLinear()
+//     .domain([0, d3.max(dataset)])
+//     .range([0, svgWidth]);
+
+// let yScale = d3.scaleLinear()
+//     .domain([0, d3.max(dataset)]) 
+//     .range([svgHeight, 0]);
+
+// let x_axis = d3.axisBottom().scale(xScale);
+// let y_axis = d3.axisLeft().scale(yScale);
+
+// svg.append("g")
+//     .attr("transform", "translate(50, 10)")
+//     .call(y_axis);
+
+// let xAxisTranslate = svgHeight - 20;
+
+// svg.append("g")
+//     .attr("transform", "translate(50, " + xAxisTranslate +")")
+//     .call(x_axis);
+//End of the scale x and y axis code 
--- index.js
+++ index.js
@@ -38,38 +38,38 @@
      
 /// THIRD TUTORIAL 
 
-// let (bargraph)dataset = [0, 250, 56, 120, 180, 30, 40 ]
+let dataset = [0, 250, 56, 120, 180, 30, 40 ]
 
 
- let dataset = [0, 16, 32, 64, 96, 112, 128, 144 ]
+//  let dataset = [0, 16, 32, 64, 96, 112, 128, 144 ]
 
 
- let dayDataset = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+//  let dayDataset = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 //  let dayDataset = [ 0, 1, 2, 3, 4, 5, 6];
 
-//  let svgWidth = 500, svgHeight = 300, barPadding = 5;
-//  let barWidth = (svgWidth / dataset.length);
+ let svgWidth = 500, svgHeight = 300, barPadding = 5;
+ let barWidth = (svgWidth / dataset.length);
 
-// let svgWidth = 500, svgHeight = 300;
+let svgWidth = 500, svgHeight = 300;
 
-//  let svg = d3.select('svg')
-//     .attr("width", svgWidth)
-//     .attr("height", svgHeight);
+ let svg = d3.select('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-// let barChart = svg.selectAll("rect")
-//     .data(dataset)
-//     .enter()
-//     .append("rect")
+let barChart = svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
 
-//     .attr("y", (data) => { return svgHeight - data})
-//     .attr("height", (data) => { return data})
+    .attr("y", (data) => { return svgHeight - data})
+    .attr("height", (data) => { return data})
 
-//     .attr("width", barWidth - barPadding)
-//     .attr("transform", (data, index) => {
-//         let translate = [barWidth * index, 0];
-//         return "translate("+translate +")"
-//     })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", (data, index) => {
+        let translate = [barWidth * index, 0];
+        return "translate("+translate +")"
+    })
  
 
 // Fourth Tutorial adding labels
@@ -91,85 +91,85 @@
 
 // Fifth Tutorial adding Scales
 
-// let xScale = d3.scaleLinear()
-//     .domain([0, d3.max(dataset)])
-//     .range([0, svgWidth]);
+let xScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgWidth]);
 
-// let yScale = d3.scaleLinear()
-//     .domain([0, d3.max(dataset)]) 
-//     .range([svgHeight, 0]);
+let yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)]) 
+    .range([svgHeight, 0]);
 
-// let x_axis = d3.axisBottom().scale(xScale);
-// let y_axis = d3.axisLeft().scale(yScale);
+let x_axis = d3.axisBottom().scale(xScale);
+let y_axis = d3.axisLeft().scale(yScale);
 
-// svg.append("g")
-//     .attr("transform", "translate(50, 10)")
-//     .call(y_axis);
+svg.append("g")
+    .attr("transform", "translate(50, 10)")
+    .call(y_axis);
 
-// let xAxisTranslate = svgHeight - 20;
+let xAxisTranslate = svgHeight - 20;
 
-// svg.append("g")
-//     .attr("transform", "translate(50, " + xAxisTranslate +")")
-//     .call(x_axis);
+svg.append("g")
+    .attr("transform", "translate(50, " + xAxisTranslate +")")
+    .call(x_axis);
 
 
     //Line Chart Tutorial 
 
 
-const drawingChart = (dataset, dayDataset) => {
-    let svgWidth = 600; 
-    let svgHeight = 400;
-    let margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    let width = svgWidth - margin.left - margin.right; 
-    let height = svgHeight - margin.top - margin.bottom;
+// const drawingChart = (dataset, dayDataset) => {
+//     let svgWidth = 600; 
+//     let svgHeight = 400;
+//     let margin = { top: 20, right: 20, bottom: 30, left: 50 };
+//     let width = svgWidth - margin.left - margin.right; 
+//     let height = svgHeight - margin.top - margin.bottom;
 
-    let svg = d3.select('svg')
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
+//     let svg = d3.select('svg')
+//     .attr("width", svgWidth)
+//     .attr("height", svgHeight);
 
-    let g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//     let g = svg.append("g")
+//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    let x = d3.scale.ordinal()
-        .domain(['Monday', 'Sunday'])
-        .range([0, width]);
+//     let x = d3.scale.ordinal()
+//         .domain(['Monday', 'Sunday'])
+//         .range([0, width]);
     
-    let y = d3.scaleLinear()
-        .rangeRound([0, height]);
+//     let y = d3.scaleLinear()
+//         .rangeRound([0, height]);
 
-    let line = d3.line()
-        .x((dayDataset) => { return x(dayDataset)})
-        .y((dataset) => { return y(dataset)})
-        x.domain(d3.extent(dayDataset, (dayDataset) => { return dayDataset}));
-        y.domain(d3.extent(dataset, (dataset) => { return dataset}));
+//     let line = d3.line()
+//         .x((dayDataset) => { return x(dayDataset)})
+//         .y((dataset) => { return y(dataset)})
+//         x.domain(d3.extent(dayDataset, (dayDataset) => { return dayDataset}));
+//         y.domain(d3.extent(dataset, (dataset) => { return dataset}));
 
-    g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .append("text")
-        .attr("fill", "#000")
-        .select(".domain")
-        .remove();
+//     g.append("g")
+//         .attr("transform", "translate(0," + height + ")")
+//         .call(d3.axisBottom(x))
+//         .append("text")
+//         .attr("fill", "#000")
+//         .select(".domain")
+//         .remove();
 
-    g.append("g")
-        .call(d3.axisLeft(y))
-        .append("text")
-        .attr("fill", "#000")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "1em")
-        .attr("text-anchor", "end")
-        .text("Water (Cups)");
+//     g.append("g")
+//         .call(d3.axisLeft(y))
+//         .append("text")
+//         .attr("fill", "#000")
+//         .attr("transform", "rotate(-90)")
+//         .attr("y", 6)
+//         .attr("dy", "1em")
+//         .attr("text-anchor", "end")
+//         .text("Water (Cups)");
 
-    g.append("g")
-        .datum(dataset)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 1.5)
-        .attr("d", line);  
+//     g.append("g")
+//         .datum(dataset)
+//         .attr("fill", "none")
+//         .attr("stroke", "steelblue")
+//         .attr("stroke-linejoin", "round")
+//         .attr("stroke-linecap", "round")
+//         .attr("stroke-width", 1.5)
+//         .attr("d", line);  
 
-}
+// }
 
-drawingChart(dataset, dayDataset)
+// drawingChart(dataset, dayDataset)
